cc4e556a05095f96b458a9130c69da0a6454d11c
Jack Lawthom <jlawthom@liveperson.com>
2021-11-20 22:29:47 +0000

    code flow

--- CompTrak/Login/Login.js
+++ CompTrak/Login/Login.js
@@ -1,5 +1,5 @@
 // Set redirect to Auth0 login
-window.location = "https://comp-trak.eu.auth0.com/authorize?response_type=token&client_id=ogt7KVEGbqnJjQ6esP1WHF7m7gGD3Gf4&redirect_uri=https://jlawthom.github.io/lawthom-tech/CompTrak/Auth/Login_Redirect.html";
+window.location = "https://comp-trak.eu.auth0.com/authorize?response_type=code&client_id=ogt7KVEGbqnJjQ6esP1WHF7m7gGD3Gf4&redirect_uri=https://jlawthom.github.io/lawthom-tech/CompTrak/Auth/Login_Redirect.html";
 
 function registerPage() {
     window.location = ACTIVE_DIR + REGISTER_HTML;
