1a8fbb26f00b576149eae02e50fa1d3eb61ff93e
Jack Lawthom <jlawthom@liveperson.com>
2021-11-20 19:18:57 +0000

    api test

--- CompTrak/Tournament Tree/Tournament Tree.js
+++ CompTrak/Tournament Tree/Tournament Tree.js
@@ -282,6 +282,7 @@ function progressPlayer(row, col, player) {
 
   // Update Database
   let matchIds = [];
+  console.log("Posting!");
   makeRequest("POST", GET_MATCH + API_CALLER, JSON.stringify({ tournamentId })).then((value) => {
     for (let dbMatch of value) {
       matchIds.push(dbMatch.matchId)
